fdf0f0888aaf7b886b4a306278a33c06ac54e52f
Manajo22 <mjoshi01@risd.edu>
2025-04-15 12:09:55 +0530

    final
    main commit

--- 2010s/script.js
+++ 2010s/script.js
@@ -1,4 +1,21 @@
-function toggleContent() {
-	let content = document.querySelector("#content");
-	content.classList.toggle('hidden');
-}
+const flashlight = document.querySelector('.flashlight');
+		const glowZone = document.querySelector('.glow-zone');
+
+
+		document.addEventListener('scroll', () => {
+			const glowTop = glowZone.offsetTop;
+			const scrollY = window.scrollY;
+			const windowHeight = window.innerHeight;
+
+
+			if (scrollY + windowHeight > glowTop + 100) {
+				flashlight.style.display = 'block';
+			} else {
+				flashlight.style.display = 'none';
+			}
+		});
+
+		document.addEventListener('mousemove', (event) => {
+			flashlight.style.left = event.clientX + 'px';
+			flashlight.style.top = event.clientY + 'px';
+		});
